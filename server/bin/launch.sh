bb62db2b4e70068d8e1533dda24e53590ac68130
Geoffrey Tipton <tipton@mysecretroom.com>
2018-09-19 13:22:30 +0800

    deployment scripting

--- server/bin/launch.sh
+++ server/bin/launch.sh
@@ -2,19 +2,20 @@
 echo "[P1] Restart is :$1 "
 
 echo "If restart, then need to stop the service, and re-establish a the deamon"
-if [ $1 -eq 1 ]
+if [ -z "$*" ]
 then
-        echo "Restarting services and killing old ones"
-        forever stopall
-        npm rebuild zeromq
-        cd ./src
-        forever start -a -o >(logger -t noderest.log) -e >(logger -t noderest.log) index.js &&
+    echo "No Arguments; which is a basic restart"
+else
+    echo "Restarting services and killing old ones; If we have multiple Arguments, then case it here."
+    forever stopall
+    npm rebuild zeromq
 fi
 
-ps auxw | grep aen.server | grep -v grep > /dev/null
-#if [ $? != 0 ]
-#then
-#        echo "Launching Chain Node"
-#         rm -f ../data/file.lock
-#         ./aen.server ../ 2>&1 | logger
-#fi
+echo "Check the serive is running, if not, then start it."
+ps auxw | grep restnode | grep -v grep > /dev/null
+if [ $? != 0 ]
+then
+    echo "Launching Rest Node Application"
+    cd ./src
+    forever start -a -o >(logger -t restnode.log) -e >(logger -t restnode.log) restnode.js &
+fi
